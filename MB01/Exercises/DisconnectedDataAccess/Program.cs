9c7ea046dcc2ec29b6bb3c0e645511897faeafd3
YourBrainEatsYou <me@yourbraineatsyou.ch>
2025-11-10 18:18:29 +0100

    implement rep

--- MB01/Exercises/DisconnectedDataAccess/Program.cs
+++ MB01/Exercises/DisconnectedDataAccess/Program.cs
@@ -1,9 +1,53 @@
-﻿
+﻿using System.Data;
+using System.Data.OleDb;
+
 public class DumpDB
 {
 
     public static void Main(string[] args)
     {
 
+        args = ["Contact"];
+
+        if (args.Length == 0)
+        {
+
+            Console.WriteLine("no args provided");
+            return;
+        }
+
+        DataSet dataSet = new();
+
+        OleDbConnection connection = new($"Provider=SQLOLEDB;Data Source=(local);Integrated Security=SSPI;Initial Catalog=playground;");
+        OleDbDataAdapter adapter = new();
+
+        foreach (var tableName in args)
+        {
+            adapter.SelectCommand = new OleDbCommand($"SELECT * FROM {tableName}", connection);
+            adapter.Fill(dataSet, tableName);
+        }
+        connection.Close();
+
+        foreach (DataTable table in dataSet.Tables)
+        {
+            Console.WriteLine(table.TableName);
+            foreach (DataColumn col in table.Columns)
+            {
+                Console.Write($"{col.ColumnName}\t");
+            }
+            Console.WriteLine();
+
+            foreach (DataRow row in table.Rows)
+            {
+                foreach (var obj in row.ItemArray)
+                {
+                    Console.Write($"{obj}\t");
+                }
+                Console.WriteLine();
+            }
+        }
+
+
     }
+
 }
--- MB01/Exercises/Repetition/Program.cs
+++ MB01/Exercises/Repetition/Program.cs
@@ -1,12 +1,37 @@
-﻿
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Data;
 
 namespace AdoNetRep
 {
-    class Program 
+    class Program
     {
         static void Main(string[] args)
         {
-           
+            Console.Write("Enter Filter-Criteria: ");
+            string filterCriteria = Console.ReadLine();
+
+            SqlConnection connection = new("Data Source=.;Initial Catalog=Northwind;Integrated Security=True;Encrypt=False");
+
+            SqlDataAdapter adapter = new();
+
+            DataSet dataSet = new();
+
+            adapter.SelectCommand = new SqlCommand($"SELECT * FROM Customers WHERE ContactName LIKE '%{filterCriteria}%' OR CompanyName LIKE '%{filterCriteria}%'", connection);
+
+            adapter.Fill(dataSet, "Customers");
+
+            foreach (DataTable table in dataSet.Tables)
+            {
+                foreach (DataRow row in table.Rows)
+                {
+                    Console.WriteLine($"{row["CustomerId"]}\t\t");
+                    Console.Write($"{row["ContactName"]}\t\t");
+                    Console.Write($"{row["CompanyName"]}\t\t");
+                }
+            }
+
+
         }
     }
 }
